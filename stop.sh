2a44d6eb27631a5613fb8898db54877a725109eb
Ryan Reichenberg <ryan.reichenberg@gmail.com>
2020-02-29 14:45:09 +1100

    Oops, uncomment commands in stop.sh

--- stop.sh
+++ stop.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-# echo Stopping containers
-# docker stop webserver db app
-# echo Removing containers
-# docker rm webserver db app
-# echo Removing images
+echo Stopping containers
+docker stop webserver db app
+echo Removing containers
+docker rm webserver db app
+echo Removing images
 if [ "$1" == "--clean-up" ]; then
     docker rmi php_service php:7.2-fpm nginx:alpine mysql:5.7.22
     dangling=$(docker images -f "dangling=true" -q)
